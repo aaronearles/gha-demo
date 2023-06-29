c64ae52ae6e2595d74484ac57670fa469132a512
Aaron Earles <aaron.earles@pediatrix.com>
2023-06-29 19:56:38 -0400

    hardcoding backend config for now

--- main.tf
+++ main.tf
@@ -8,7 +8,13 @@ provider "azurerm" {
 
 terraform {
   backend "azurerm" {
-    //see backend-config.tfvars
+    resource_group_name  = "stateful-rg"
+    storage_account_name = "aearles00"
+    container_name       = "terraform"
+    use_azuread_auth     = true
+    subscription_id      = "4fa877d6-c808-415e-93bd-2be1969e7f3a" //Pay-As-You-Go
+    tenant_id            = "2f9dc271-8ef4-4ba2-84a3-420e7837b641" //Aaron Personal Tenant
+    key                  = "gha-demo"
   }
 }
 
