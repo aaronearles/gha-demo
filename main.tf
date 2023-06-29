9fb33a15ea8c9e9bc0b15e15af6b7e9c9e5a5843
Aaron Earles <aaron.earles@pediatrix.com>
2023-06-29 18:31:51 -0400

    updates

--- main.tf
+++ main.tf
@@ -8,23 +8,11 @@ provider "azurerm" {
 
 terraform {
   backend "azurerm" {
-    resource_group_name  = "stateful-rg"
-    storage_account_name = "aearles00"
-    container_name       = "terraform"
-    use_azuread_auth     = true
-    subscription_id      = "4fa877d6-c808-415e-93bd-2be1969e7f3a" //Pay-As-You-Go
-    tenant_id            = "2f9dc271-8ef4-4ba2-84a3-420e7837b641" //Aaron Personal Tenant
-    key                  = "gha-demo"
+  //see backend-config.tfvars
   }
 }
 
-data "azurerm_resource_group" "rg" {
+resource "azurerm_resource_group" "rg" {
   name     = var.resource_group_name
-}
-
-resource "azurerm_management_lock" "rglock" {
-  name       = var.rglock_name
-  scope      = data.azurerm_resource_group.rg.id
-  lock_level = var.lock_level
-  notes      = var.lock_notes
+  location = var.location
 }
