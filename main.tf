f599dd7a21a60ce95dd1666676b3ce23f11873b3
Aaron Earles <aaron.earles@pediatrix.com>
2023-06-29 19:57:44 -0400

    -use_azuread_auth

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ terraform {
     resource_group_name  = "stateful-rg"
     storage_account_name = "aearles00"
     container_name       = "terraform"
-    use_azuread_auth     = true
+    # use_azuread_auth     = true
     subscription_id      = "4fa877d6-c808-415e-93bd-2be1969e7f3a" //Pay-As-You-Go
     tenant_id            = "2f9dc271-8ef4-4ba2-84a3-420e7837b641" //Aaron Personal Tenant
     key                  = "gha-demo"
