04bc890bf6af61421ae1b229ce154a4d8fdd1ebe
Aaron Earles <aaron.earles@pediatrix.com>
2023-06-29 20:11:27 -0400

    attempt run after duplicate secrets to env

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ terraform {
     container_name       = "terraform"
     # use_azuread_auth     = true
     subscription_id = "4fa877d6-c808-415e-93bd-2be1969e7f3a" //Pay-As-You-Go
-    tenant_id       = "2f9dc271-8ef4-4ba2-84a3-420e7837b641" //Aaron Personal Tenant
+    tenant_id       = "2f9dc271-8ef4-4ba2-84a3-420e7837b641" //Aaron Personal Tenant 
     key             = "gha-demo"
   }
 }
