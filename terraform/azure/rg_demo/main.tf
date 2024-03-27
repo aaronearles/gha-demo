96f5a0fdf83cc8b1a836ed4e158cd75034271742
Aaron Earles <aaron.earles@pediatrix.com>
2024-03-27 13:54:24 -0600

    fmt

--- terraform/azure/rg_demo/main.tf
+++ terraform/azure/rg_demo/main.tf
@@ -21,7 +21,7 @@ terraform {
     # use_azuread_auth     = true
     subscription_id = "4fa877d6-c808-415e-93bd-2be1969e7f3a" //Pay-As-You-Go
     tenant_id       = "2f9dc271-8ef4-4ba2-84a3-420e7837b641" //Aaron Personal Tenant 
-    key = "rg_demo"
+    key             = "rg_demo"
   }
 }
 
